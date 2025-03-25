cc199fdcaa9c41e8f33eaa611a0c0e6798513900
mfikih <mfikih@movensys.com>
2025-03-25 18:11:44 +0900

    haha

--- ros2_rust/launch/haha.launch.py
+++ ros2_rust/launch/haha.launch.py
@@ -1,1 +1,18 @@
-#
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
+from launch.event_handlers import OnProcessExit
+from ament_index_python.packages import get_package_share_directory
+
+start_publisher = Node(package='ros2_rust', executable='publisher.py', name='publisher', output='screen')
+start_subscriber = Node(package='ros2_rust', executable='subscriber.py', name='subscriber', output='screen')
+             
+def generate_launch_description():
+    return LaunchDescription([
+        start_publisher,
+        start_subscriber 
+    ])
