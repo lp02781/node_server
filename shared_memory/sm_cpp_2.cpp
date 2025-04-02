bf139fafe17d3199f4a9786709aec10daf19770e
Fikih Muhamad <mfikih@movensys.com>
2025-04-02 17:34:16 +0900

    haha

--- shared_memory/sm_cpp_2.cpp
+++ shared_memory/sm_cpp_2.cpp
@@ -7,19 +7,19 @@
 #include <random>
 
 int main() {
-    iox::runtime::PoshRuntime::initRuntime("sm_1");
+    iox::runtime::PoshRuntime::initRuntime("sm_cpp_2");
 
-    iox::capro::ServiceDescription temperature_service("sensor_data", "sm_1", "temperature");
-    iox::capro::ServiceDescription humidity_service("sensor_data", "sm_1", "humidity");
-    iox::capro::ServiceDescription current_service("sensor_data", "sm_1", "current");
+    iox::capro::ServiceDescription temperature_service("sensor_data", "sm_cpp_2", "temperature");
+    iox::capro::ServiceDescription humidity_service("sensor_data", "sm_cpp_2", "humidity");
+    iox::capro::ServiceDescription current_service("sensor_data", "sm_cpp_2", "current");
 
     iox::popo::Publisher<float> pub_temperature(temperature_service);
     iox::popo::Publisher<int> pub_humidity(humidity_service);
     iox::popo::Publisher<float> pub_current(current_service);
 
-    iox::popo::Subscriber<float> sub_temperature({"sensor_data", "sm_actix", "temperature"});
-    iox::popo::Subscriber<int> sub_humidity({"sensor_data", "sm_actix", "humidity"});
-    iox::popo::Subscriber<float> sub_current({"sensor_data", "sm_actix", "current"});
+    iox::popo::Subscriber<float> sub_temperature({"sensor_data", "sm_cpp_1", "temperature"});
+    iox::popo::Subscriber<int> sub_humidity({"sensor_data", "sm_cpp_1", "humidity"});
+    iox::popo::Subscriber<float> sub_current({"sensor_data", "sm_cpp_1", "current"});
 
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -27,7 +27,7 @@ int main() {
     std::uniform_int_distribution<int> humidity_dist(30, 80);
     std::uniform_real_distribution<float> current_dist(1.0, 10.0);
 
-    std::cout << "[sm_1] sm_1 is ready" << std::endl;
+    std::cout << "[sm_cpp_2] sm_cpp_2 is ready" << std::endl;
 
     while (true) {
         float temperature = temp_dist(gen);
@@ -52,18 +52,18 @@ int main() {
                 sample.publish();
             });
 
-        std::cout << "[sm_1] Publishing temp: " << temperature 
+        std::cout << "[sm_cpp_2] Publishing temp: " << temperature 
                   << " hum: " << humidity 
                   << " current: " << current << std::endl;
 
         if (auto sample = sub_temperature.take()) {
-            std::cout << "[sm_1] Received temp: " << **sample << std::endl;
+            std::cout << "[sm_cpp_2] Received temp: " << **sample << std::endl;
         }
         if (auto sample = sub_humidity.take()) {
-            std::cout << "[sm_1] Received hum: " << **sample << std::endl;
+            std::cout << "[sm_cpp_2] Received hum: " << **sample << std::endl;
         }
         if (auto sample = sub_current.take()) {
-            std::cout << "[sm_1] Received current: " << **sample << std::endl;
+            std::cout << "[sm_cpp_2] Received current: " << **sample << std::endl;
         }
 
         std::this_thread::sleep_for(std::chrono::seconds(10));
---
+++ shared_memory/sm_rust_1.rs
@@ -0,0 +1,77 @@
+use core::time::Duration;
+use iceoryx2::prelude::*;
+use rand::Rng; 
+
+const CYCLE_TIME: Duration = Duration::from_secs(10);
+
+fn main() -> Result<(), Box<dyn core::error::Error>> {
+    let node = NodeBuilder::new().create::<ipc::Service>()?;
+
+    let service_temperature = node
+        .service_builder(&"sensor_data/sm_rust_1/temperature".try_into()?)
+        .publish_subscribe::<f32>() 
+        .open_or_create()?;
+
+    let service_humidity = node
+        .service_builder(&"sensor_data/sm_rust_1/humidity".try_into()?)
+        .publish_subscribe::<i16>() 
+        .open_or_create()?;
+
+    let service_current = node
+        .service_builder(&"sensor_data/sm_rust_1/current".try_into()?) 
+        .publish_subscribe::<f32>() 
+        .open_or_create()?;
+
+    let publisher_temperature = service_temperature.publisher_builder().create()?;
+    let publisher_humidity = service_humidity.publisher_builder().create()?;
+    let publisher_current = service_current.publisher_builder().create()?;
+
+    let sub_temperature = node
+        .service_builder(&"sensor_data/sm_rust_2/temperature".try_into()?)
+        .publish_subscribe::<f32>()
+        .open_or_create()?;
+
+    let sub_humidity = node
+        .service_builder(&"sensor_data/sm_rust_2/humidity".try_into()?)
+        .publish_subscribe::<i16>()
+        .open_or_create()?;
+
+    let sub_current = node
+        .service_builder(&"sensor_data/sm_rust_2/current".try_into()?)
+        .publish_subscribe::<f32>()
+        .open_or_create()?;
+
+    let subscriber_temperature = sub_temperature.subscriber_builder().create()?;
+    let subscriber_humidity = sub_humidity.subscriber_builder().create()?;
+    let subscriber_current = sub_current.subscriber_builder().create()?;
+
+    let mut rng = rand::thread_rng(); 
+
+    while node.wait(CYCLE_TIME).is_ok() {
+        let temperature = rng.gen_range(15.0..40.0);
+        let humidity = rng.gen_range(30..80); 
+        let current = rng.gen_range(1.0..10.0);
+
+        publisher_temperature.loan_uninit()?.write_payload(temperature).send()?;
+        publisher_humidity.loan_uninit()?.write_payload(humidity).send()?;
+        publisher_current.loan_uninit()?.write_payload(current).send()?;
+
+        println!(
+            "[sm_rust_1] Published → temp: {:.2}, hum: {}, current: {:.2}",
+            temperature, humidity, current
+        );
+
+        if let Some(sample) = subscriber_temperature.receive()? {
+            println!("[sm_rust_1] Received temp: {:.2}", *sample);
+        }
+        
+        if let Some(sample) = subscriber_humidity.receive()? {
+            println!("[sm_rust_1] Received hum: {:.2}", *sample);
+        }
+        
+        if let Some(sample) = subscriber_current.receive()? {
+            println!("[sm_rust_1] Received cur: {:.2}", *sample);
+        }
+    }
+    Ok(())
+}
