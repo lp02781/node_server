5aa031abe800140b7174fd06eac3aee3c7fecf4e
mfikih <mfikih@movensys.com>
2025-04-17 13:33:14 +0900

    repair

--- deploy/restart_node_server.sh
+++ deploy/restart_node_server.sh
@@ -6,7 +6,7 @@ GREEN='\e[32m'
 echo "${GREEN}Restart container${NC}"
 #docker restart node_sm_rust_container
 docker restart node_actix_container
-docker restart node_webscoket_container
+docker restart node_websocket_container
 docker restart node_tcp_container
 docker restart node_sm_cpp_container
 docker restart node_mosquitto_container
