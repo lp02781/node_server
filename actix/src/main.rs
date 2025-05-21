90b42fa8414c7bba8b3ac9eaa11df9de25208605
mfikih <mfikih@movensys.com>
2025-05-21 10:32:16 +0900

    repair tokio

--- actix/src/main.rs
+++ actix/src/main.rs
@@ -7,34 +7,38 @@ mod tcp;
 mod websocket;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {   
-    tokio::spawn(async {mqtt::start_mqtt_1_subscriber().await;});
-    tokio::spawn(async {mqtt::start_mqtt_2_subscriber().await;});
-    tokio::spawn(async {mqtt::start_mqtt_actix_publisher().await;});
-    tokio::spawn(async {tcp::start_tcp_actix().await;});
-    //tokio::spawn(async {ros2::start_ros2_publisher().await;});
+async fn main() -> std::io::Result<()> {
+    let _ = tokio::spawn(mqtt::start_mqtt_1_subscriber());
+    let _ = tokio::spawn(mqtt::start_mqtt_2_subscriber());
+    let _ = tokio::spawn(mqtt::start_mqtt_actix_publisher());
+    let _ = tokio::spawn(tcp::start_tcp_actix());
+    // let _ = tokio::spawn(ros2::start_ros2_publisher()); 
 
     HttpServer::new(move || {
         App::new()
-        .route("/node/{device}/data", web::post().to(receive_device_data))
-        .route("/ws/{device}", web::get().to(websocket_handler))
+            .route("/node/{device}/data", web::post().to(receive_device_data))
+            .route("/ws/{device}", web::get().to(websocket_handler))
     })
     .bind(("127.0.0.1", 5000))?
     .run()
     .await
 }
 
-async fn receive_device_data( device: web::Path<String>,
-                              payload: web::Json<json::NodePayload>) 
--> impl Responder {
+async fn receive_device_data(
+    device: web::Path<String>,
+    payload: web::Json<json::NodePayload>,
+) -> impl Responder {
     println!("Received data for device: {}", device);
     println!("Payload: {:?}", payload);
-    
+
     HttpResponse::Ok().json(payload.into_inner())
 }
 
-async fn websocket_handler(req: HttpRequest, stream: web::Payload, device: web::Path<String>) 
--> actix_web::Result<HttpResponse> {
+async fn websocket_handler(
+    req: HttpRequest,
+    stream: web::Payload,
+    device: web::Path<String>,
+) -> actix_web::Result<HttpResponse> {
     let device_id = device.into_inner();
     let ws = websocket::WebSocketSession { device_id };
     ws::start(ws, &req, stream)
