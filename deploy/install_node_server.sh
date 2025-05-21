2c67309af8c34422d6181392e814f10c482155c1
lp02781 <lp02781@gmail.com>
2025-05-21 20:06:22 +0900

    haha

--- deploy/install_node_server.sh
+++ deploy/install_node_server.sh
@@ -9,5 +9,14 @@ sudo ./uninstall_node_server.sh
 echo "${GREEN}Build and run docker compose${NC}"
 docker-compose -f compose_node_server.yml up -d
 
+echo"${GREEN} Configure database"
+docker exec -i node_postgres_container psql -U admin_haha -d db_haha <<EOF
+CREATE TABLE websocket (timestamp DOUBLE PRECISION, temperature REAL, humidity INTEGER, current REAL);
+CREATE TABLE tcp (timestamp DOUBLE PRECISION, temperature REAL, humidity INTEGER, current REAL);
+CREATE TABLE sm_cpp (timestamp DOUBLE PRECISION, temperature REAL, humidity INTEGER, current REAL);
+CREATE TABLE sm_rust (timestamp DOUBLE PRECISION, temperature REAL, humidity INTEGER, current REAL);
+CREATE TABLE mqtt (timestamp DOUBLE PRECISION, temperature REAL, humidity INTEGER, current REAL);
+EOF
+
 echo "${GREEN}Restart container${NC}"
 sudo ./restart_node_server.sh
