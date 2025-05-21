401231857065e890f4b53bb4023866458c92e8cf
lp02781 <lp02781@gmail.com>
2025-05-21 20:33:18 +0900

    haha

--- actix/src/json.rs
+++ actix/src/json.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct NodePayload {
     pub id: String,
     pub timestamp: f64,
     pub data: SensorData,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct SensorData {
     pub temperature: f32,
     pub humidity: u32,
